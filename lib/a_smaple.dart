852167c22c600b7ec6138ce2c3fd040bac2334a0
Peter-Mac <peter@vyrox.com>
2023-02-15 20:59:41 +0800

    Sample.dart added.

---
+++ lib/a_smaple.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//? Peter: Sample Project start.
+class SampleWidget extends StatefulWidget {
+  const SampleWidget({super.key});
+
+  @override
+  State<SampleWidget> createState() => _SampleWidgetState();
+}
+
+class _SampleWidgetState extends State<SampleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+//? Peter: Sample Project end.
+
