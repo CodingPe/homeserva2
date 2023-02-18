f1c536eb77e03108ed1c9ce65631d4260ce6536b
Peter-Mac <peter@vyrox.com>
2023-02-18 18:45:32 +0800

    //Optimized some code and function/widget name.

--- lib/a10_00_dashboard.dart
+++ lib/a10_00_dashboard.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:firebase_messaging/firebase_messaging.dart'; //push notification必定需要安裝的package //push notification must be installed package
-import 'package:firebase_core/firebase_core.dart';
-import 'dart:convert';
-import 'firebase_options.dart';
+//? firebase package not using at this file, dart:covert duplicated.
+// import 'package:firebase_messaging/firebase_messaging.dart'; //push notification必定需要安裝的package //push notification must be installed package
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key, String? a});
 
-  Future getannouncmentData() async {
+  Future getAnnouncementData() async {
     var url = 'https://peterapi.vyrox.com/viewannouncementsdata.php';
     var response = await http.get(Uri.parse(url));
     return json.decode(response.body);
@@ -18,97 +18,92 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.home),
-        title: const Text("Dashboard"),
-      ),
-      body: Column(
-        children: <Widget>[
+        appBar: AppBar(
+          leading: const Icon(Icons.home),
+          title: const Text("Dashboard"),
+        ),
+        body: Column(children: <Widget>[
           Container(
-            margin: EdgeInsets.only(top: 10,right: 160),
-            child: RichText(
-              text: const TextSpan(
-                children: [
-                  WidgetSpan(
-                    child: Icon(Icons.speaker_notes, size: 20,color: Colors.black),
-                  ),
-                  TextSpan(
-                      text: "Announcements",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w200,
-                          fontSize: 20
-                      )
-                  ),
-                ],
+              margin: const EdgeInsets.only(top: 10, right: 160),
+              child: RichText(
+                  text: const TextSpan(children: [
+                WidgetSpan(
+                    child: Icon(Icons.speaker_notes,
+                        size: 20, color: Colors.black)),
+                TextSpan(
+                    text: "Announcements",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w200,
+                        fontSize: 20))
+              ])) //
               ),
-            ), //
-          ),
           Expanded(
-            flex: 2,
-            child: FutureBuilder(
-              future: getannouncmentData(),
-              builder: (context, snapshot){
-                if(snapshot.hasError) print(snapshot.error);
-                return snapshot.hasData ? ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (context,index){
-                      List list = snapshot.data;
-                      return Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: GestureDetector(
-                          onTap: (){
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DetailAnnouncementPage(list: list, index: index),
-                              ),
-                            );
-                          },
-                          child: Row(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(12.0),
-                                child: Image.network(
-                                  list[index]['Photo'],
-                                  width: 70,
-                                  height: 70,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              const SizedBox(width: 16),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      list[index]['Title'],
-                                      style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                      list[index]['Time'],
-                                      style: const TextStyle(fontWeight: FontWeight.w200, color: Color.fromARGB(255, 66, 72, 82),fontSize: 13),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-                ) : const Center(child: CircularProgressIndicator(),);
-              },
-            ) //這個是announcement的futurebuilder 可以通過上面的flex改變大小
-          ),
+              flex: 2,
+              child: FutureBuilder(
+                future: getAnnouncementData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) print(snapshot.error);
+                  return snapshot.hasData
+                      ? ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) {
+                            List list = snapshot.data;
+                            return Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  DetailAnnouncementPage(
+                                                      list: list,
+                                                      index: index)));
+                                    },
+                                    child: Row(children: [
+                                      ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(12.0),
+                                          child: Image.network(
+                                              list[index]['Photo'],
+                                              width: 70,
+                                              height: 70,
+                                              fit: BoxFit.cover)),
+                                      const SizedBox(width: 16),
+                                      Expanded(
+                                          child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                            Text(
+                                              list[index]['Title'],
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            Text(
+                                              list[index]['Time'],
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.w200,
+                                                  color: Color.fromARGB(
+                                                      255, 66, 72, 82),
+                                                  fontSize: 13),
+                                            )
+                                          ]))
+                                    ])));
+                          })
+                      : const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                },
+              ) //這個是announcement的futurebuilder 可以通過上面的flex改變大小
+              ),
           Expanded(
-            flex: 3,
-            child: Container(), //下面還不知道要放什麼下去
-          ),
-        ],
-      )
-    );
+            flex: 3, child: Container(), //下面還不知道要放什麼下去
+          )
+        ]));
   }
 }
 
@@ -116,31 +111,33 @@ class DetailAnnouncementPage extends StatelessWidget {
   final List list;
   final int index;
 
-  const DetailAnnouncementPage({Key? key, required this.list, required this.index}) : super(key: key);
+  const DetailAnnouncementPage(
+      {Key? key, required this.list, required this.index})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(list[index]['Title'], style: const TextStyle(fontWeight: FontWeight.bold)),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            children: <Widget>[
+        appBar: AppBar(
+            title: Text(list[index]['Title'],
+                style: const TextStyle(fontWeight: FontWeight.bold))),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+                child: Column(children: <Widget>[
               Image.network(list[index]['Photo']),
               const SizedBox(height: 16.0),
-              Text(list[index]['Title'],style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 30),textAlign: TextAlign.center,),
+              Text(list[index]['Title'],
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 30),
+                  textAlign: TextAlign.center),
               const SizedBox(height: 16.0),
-              Text(list[index]['Description'],textAlign: TextAlign.center,),
+              Text(list[index]['Description'], textAlign: TextAlign.center),
               const SizedBox(height: 16.0),
-              Text(list[index]['Time'], style: const TextStyle(fontWeight: FontWeight.w300),),
-              const SizedBox(height: 16.0),
-            ],
-          ), //可以繼續加自己想要的東西在Column裡面
-        ),
-      ),
-    );
+              Text(list[index]['Time'],
+                  style: const TextStyle(fontWeight: FontWeight.w300)),
+              const SizedBox(height: 16.0)
+            ]) //可以繼續加自己想要的東西在Column裡面
+                )));
   }
 } //當用戶點擊了announcement，他將會看到關於有關announcement的詳情，包括時間，主題，詳細內容和照片
