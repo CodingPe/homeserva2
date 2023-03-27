3623d064c502a29fdcd5557880c670d22b5659aa
Peter-Mac <peter@vyrox.com>
2023-03-27 23:01:37 +0800

    Notification render list.

--- lib/a40_00_notification.dart
+++ lib/a40_00_notification.dart
@@ -3,10 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future getNotificationsData() async {
-  var url = 'https://peterapi.vyrox.com/viewnotificationsdata.php';
-  var response = await http.get(Uri.parse(url));
-  return json.decode(response.body);
+abstract class AboutNotification {
+  List<Widget> items = <Widget>[];
+
+  Future getNotificationsData() async {
+    var url = 'https://peterapi.vyrox.com/viewnotificationsdata.php';
+    var response = await http.get(Uri.parse(url));
+    return json.decode(response.body);
+  }
 }
 
 //to create a notification page, which can pull to show cupertinoactivitycircle with bouncing effect, and fetch latest notification, pull_to_refresh_flutter3
@@ -15,31 +19,23 @@ class Notifications extends StatefulWidget {
   const Notifications({super.key});
 
   @override
-  State<Notifications> createState() => _NotificationsState();
+  State<Notifications> createState() => NotificationsState();
 }
 
-class _NotificationsState extends State<Notifications> {
-  List<Color> colors = <Color>[
-    CupertinoColors.systemYellow,
-    CupertinoColors.systemOrange,
-    CupertinoColors.systemPink,
-  ];
-  List<Widget> items = <Widget>[
-    Container(color: CupertinoColors.systemPink, height: 80.0),
-    Container(color: CupertinoColors.systemOrange, height: 80.0),
-    Container(color: CupertinoColors.systemYellow, height: 80.0),
-  ];
-
-  Future<void> aa() async {
+class NotificationsState extends State<Notifications> with AboutNotification {
+  Future<void> refreshAction() async {
     await Future<void>.delayed(
       const Duration(milliseconds: 1000),
     );
     setState(() {
       //TODO: fetch latest notification from database, and insert into items.
-      // items.insert(
-      //   0,
-      //   Container(color: colors[items.length % 3], height: 80.0),
-      // );
+      items.insert(
+          0,
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: Text('Testing ${items.length + 1}'),
+            subtitle: const Text('Testing'),
+          ));
     });
   }
 
@@ -47,25 +43,91 @@ class _NotificationsState extends State<Notifications> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Notifications'),
-      ),
+          leading: const Icon(Icons.home),
+          title: const Text('Notifications'),
+          actions: [
+            SizedBox(
+                width: 35,
+                height: 35,
+                child: CircleAvatar(
+                    backgroundColor: Colors.red,
+                    child: FloatingActionButton(
+                        backgroundColor: Colors.red,
+                        onPressed: () {},
+                        child: const Text('SOS',
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 11))))),
+            const SizedBox(width: 10),
+          ]),
       body: CustomScrollView(
         physics: const BouncingScrollPhysics(
           parent: AlwaysScrollableScrollPhysics(),
         ),
         slivers: <Widget>[
           CupertinoSliverRefreshControl(
-            onRefresh: aa,
+            onRefresh: refreshAction,
           ),
-          SliverList(
-            //TODO: fetch notification from database, and insert into items.
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) => items[index],
-              childCount: items.length,
-            ),
-          ),
+          items.isEmpty
+              ? const EmptyList()
+              : SliverList(
+                  //TODO: fetch notification from database, and insert into items.
+                  delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, index) => items[index],
+                      childCount: items.length)),
         ],
       ),
     );
+  }
+}
+
+class EmptyList extends StatelessWidget {
+  const EmptyList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverFillRemaining(
+        child: Center(
+            child: Text('- No notification -',
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w300))));
+  }
+}
+
+class DetailNotificationsPage extends StatelessWidget {
+  final List list;
+  final int index;
+
+  const DetailNotificationsPage(
+      {Key? key, required this.list, required this.index})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(list[index]['Title'],
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+                child: Column(children: <Widget>[
+              Image.network(list[index]['Photo']),
+              const SizedBox(height: 16.0),
+              Text(list[index]['Title'],
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 30),
+                  textAlign: TextAlign.center),
+              const SizedBox(height: 16.0),
+              Text(list[index]['Body'], textAlign: TextAlign.center),
+              const SizedBox(height: 16.0),
+              Text(list[index]['Time'],
+                  style: const TextStyle(fontWeight: FontWeight.w300)),
+              const SizedBox(height: 16.0),
+              //可以繼續加自己想要的東西在Column裡面
+            ]))));
   }
 }
