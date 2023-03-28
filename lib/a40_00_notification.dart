c83bde9aaddf7ee3705cba065ce132bc042fa34c
Peter-Mac <peter@vyrox.com>
2023-03-29 00:49:25 +0800

    notification added badge.

--- lib/a40_00_notification.dart
+++ lib/a40_00_notification.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:badges/badges.dart' as badges;
 import 'dart:convert';
 
 abstract class AboutNotification {
   List<Widget> items = <Widget>[];
+  int notificationCount = 0;
+  final abc = ValueNotifier<int>(Hive.box('TokenBox').get(2));
 
   Future getNotificationsData() async {
     var url = 'https://peterapi.vyrox.com/viewnotificationsdata.php';
@@ -28,7 +33,11 @@ class NotificationsState extends State<Notifications> with AboutNotification {
       const Duration(milliseconds: 1000),
     );
     setState(() {
-      //TODO: fetch latest notification from database, and insert into items.
+      notificationCount = items.length;
+      Hive.box('TokenBox').put(2, notificationCount);
+      //!notification no refresh
+      abc.value++;
+      print('notificationCount: ${Hive.box('TokenBox').get(2)}');
       items.insert(
           0,
           ListTile(
@@ -51,7 +60,13 @@ class NotificationsState extends State<Notifications> with AboutNotification {
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var box = Hive.box('TokenBox');
     return Scaffold(
       appBar: AppBar(
           leading: const Icon(Icons.home),
@@ -104,41 +119,5 @@ class EmptyList extends StatelessWidget {
                     color: Colors.grey,
                     fontStyle: FontStyle.italic,
                     fontWeight: FontWeight.w300))));
-  }
-}
-
-class DetailNotificationsPage extends StatelessWidget {
-  final List list;
-  final int index;
-
-  const DetailNotificationsPage(
-      {Key? key, required this.list, required this.index})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(list[index]['Title'],
-              style: const TextStyle(fontWeight: FontWeight.bold)),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Center(
-                child: Column(children: <Widget>[
-              Image.network(list[index]['Photo']),
-              const SizedBox(height: 16.0),
-              Text(list[index]['Title'],
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 30),
-                  textAlign: TextAlign.center),
-              const SizedBox(height: 16.0),
-              Text(list[index]['Body'], textAlign: TextAlign.center),
-              const SizedBox(height: 16.0),
-              Text(list[index]['Time'],
-                  style: const TextStyle(fontWeight: FontWeight.w300)),
-              const SizedBox(height: 16.0),
-              //可以繼續加自己想要的東西在Column裡面
-            ]))));
   }
 }
